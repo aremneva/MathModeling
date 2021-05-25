3ac4fdcaf20232f9af7ebf932bbdc1dac820f39a
aremneva <remnevaa0@gmail.com>
2021-05-25 11:52:40 +0300

    added pages for methods

--- routes.py
+++ routes.py
@@ -14,16 +14,45 @@ def home():
         year=datetime.now().year
     )
 
-@route('/contact')
-@view('contact')
-def contact():
+@route('/BFS')
+@view('BFS')
+def bfs():
     """Renders the contact page."""
     return dict(
-        title='Contact',
-        message='Your contact page.',
+        title='BFS',
+        message='Breadth-first search',
         year=datetime.now().year
     )
 
+@route('/DFS')
+@view('DFS')
+def dfs():
+    """Renders the contact page."""
+    return dict(
+        title='DFS',
+        message='Depth-first search',
+        year=datetime.now().year
+    )
+
+@route('/Kruskal')
+@view('Kruskal')
+def kruskal():
+    """Renders the contact page."""
+    return dict(
+        title='Kruskal',
+        message='Kruskal`s algorithm',
+        year=datetime.now().year
+    )
+
+@route('/Prim')
+@view('Prim')
+def prim():
+    """Renders the contact page."""
+    return dict(
+        title='Prim',
+        message='Prim`s algorithm',
+        year=datetime.now().year
+    )
 @route('/about')
 @view('about')
 def about():
